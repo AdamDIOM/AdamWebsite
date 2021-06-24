1edd2c88120beb7e7e6492cca6483509550dbd33
AdamDIOM <adam.drummond9@gmail.com>
2021-06-24 21:53:19 +0100

    modified education section

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -7,8 +7,9 @@ export default function Education(){
     return (
         <Container className="qualifications" id="qualifications">
             <h1>Education</h1>
-            <h3 className="school">St Ninian's High School</h3>
+            <i>Section still under development</i>
             <h2>Find below the qualifications I have earned as part of my formal education</h2>
+            <h3 className="school">St Ninian's High School</h3>
 
             
             <table>
