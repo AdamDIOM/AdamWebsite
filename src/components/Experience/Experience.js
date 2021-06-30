5adc577b3a75a3ce9ae55ec721189df34c34fc3f
AdamDIOM <adam.drummond9@gmail.com>
2021-06-30 14:06:39 +0100

    added volunteer experience

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,4 +1,5 @@
 import { Container } from 'react-bootstrap';
+import Box from '../Box/Box';
 
 import './Experience.css';
 
@@ -7,6 +8,19 @@ export default function Achievements(){
         <Container className="experience" id="experience">
             <h1>Experience</h1>
             <h2>All of my work experience, both paid and voluntary is here</h2>
+
+            <Box
+                title="Isle of Man Code Club: Volunteer Educator"
+                date="2017 - Present"
+                text="I volunteer at the IOM Code Club as the Assets Manager. This means that I get to handle all the cool tech that we deal with on a weekly basis! Furthermore, as a member of the committee, I have an insight & a say into how the club is run."
+            />
+
+            <Box
+                title="Junior Achievement Ambassador"
+                date="September 2020 - June 2021"
+                text="I helped to mentor a team into the JA Company of the Year Competition 2021. After months of hard work, the team came in as THE Company of the Year."
+            />
+
         </Container>
     )
     
