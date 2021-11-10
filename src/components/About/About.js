5d112620eb201c47fc8eec5eda99971ece84b317
AdamDIOM <adam.drummond9@gmail.com>
2021-11-10 09:02:55 +0000

    updated about.js to reflect moving to uni

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,13 +6,13 @@ export default function About(){
     return (
         <Container className="about" id="about">
             <h1>About me</h1>
-            <h2>Hi, I'm Adam, a secondary school student on the Isle of Man!</h2>
+            <h2>Hi, I'm Adam, an undergraduate student on the Isle of Man!</h2>
 
-            <p>Currently, a Student at <a className="link" href="https://snhs.sch.im">St Ninian's High School</a>, Isle of Man, studying A Levels in Computer Science and Mathematics. I also study a BTEC Level 3 Extended Certificate in Enterprise and Entrepreneurship.</p>
+            <p>Currently a student at <a className="link" href="https://ucm.ac.im">University College Isle of Man</a>, studying a BSc (Hons) in Computer Science.</p>
 
-            <p>In the past, I studied GCSEs and IGCSEs accross a variety of STEM subjects. I have also undertaken a number of extra-curricular certifications across professional products, such as Microsoft Office</p>
+            <p>In the past, I studied GCSEs and IGCSEs accross a variety of STEM subjects, as well as A Levels in Mathematics and Computer Science. I have also undertaken a number of extra-curricular certifications across professional products, such as Microsoft Office</p>
 
-            <p>Out of school, I currently volunteer at the <a className="link" href="https://codeclub.im">Isle of Man Code Club</a>, where I am the official Assets Manager. This role required me to organise and categorise our large range of objects. I am also a barista at a local branch of the Costa Coffee chain.</p>
+            <p>Out of university, I currently volunteer at the <a className="link" href="https://codeclub.im">Isle of Man Code Club</a>, where I am the official Assets Manager. This role required me to organise and categorise our large range of objects. I am also a barista at a local branch of the Costa Coffee chain.</p>
             
 
         </Container>
